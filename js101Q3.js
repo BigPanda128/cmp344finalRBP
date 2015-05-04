24044a95d08c3f5f36ba98142494767a4ac39f68
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-04 14:19:41 -0400

    added some comments

--- js101Q3.js
+++ js101Q3.js
@@ -1,16 +1,22 @@
-(function(exports) {
+(function(exports) {//node module
+	//add function
 	function add(num1, num2) {
 		return (num1 + num2);
 	};
+	//subtract funtion
 	function subtract(num1, num2) {
 	        return (num1 - num2);
 	};
+	//multiply function
 	function multiply(num1, num2) {
 		return (num1 * num2);
 	};
+	//divide function
 	function divide(num1, num2) {
 		return (num1 / num2);
 	};
+
+	//export each of the names to acces the function outside
 	exports.add = add;
 	exports.subtract = subtract;
 	exports.multiply = multiply;
