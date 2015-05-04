9b571111286e81d2576a8b17905fb79bb154bb19
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-04 14:20:50 -0400

    added some comments as well

--- js101Q3Test.js
+++ js101Q3Test.js
@@ -1,8 +1,10 @@
+//import the functions and attach them to easy to know names that tell what they do
 var add = require('./js101Q3.js').add;
 var subtract = require('./js101Q3.js').subtract;
 var multiply = require('./js101Q3.js').multiply;
 var divide = require('./js101Q3.js').divide;
 
+//calling all functions
 console.log('adding 6 and 7 ' + add(6,7));
 console.log('subtracting 5 and 3 ' + subtract(5,3));
 console.log('multiplying 3 and 4 ' + multiply(3,4));
