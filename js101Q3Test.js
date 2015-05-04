2698959bc93168c2020f62a5b5e6d28317ed5294
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-04 14:03:35 -0400

    the driver code for js101Q3.js

---
+++ js101Q3Test.js
@@ -0,0 +1,9 @@
+var add = require('./js101Q3.js').add;
+var subtract = require('./js101Q3.js').subtract;
+var multiply = require('./js101Q3.js').multiply;
+var divide = require('./js101Q3.js').divide;
+
+console.log('adding 6 and 7 ' + add(6,7));
+console.log('subtracting 5 and 3 ' + subtract(5,3));
+console.log('multiplying 3 and 4 ' + multiply(3,4));
+console.log('dividing 9 and 3 ' + divide(9,3));
