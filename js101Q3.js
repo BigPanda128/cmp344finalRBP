a988d31a8910770cfe254db8a29749cb75b24ed2
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-05-04 14:01:29 -0400

    the code for javascript 101 question 3

---
+++ js101Q3.js
@@ -0,0 +1,19 @@
+(function(exports) {
+	function add(num1, num2) {
+		return (num1 + num2);
+	};
+	function subtract(num1, num2) {
+	        return (num1 - num2);
+	};
+	function multiply(num1, num2) {
+		return (num1 * num2);
+	};
+	function divide(num1, num2) {
+		return (num1 / num2);
+	};
+	exports.add = add;
+	exports.subtract = subtract;
+	exports.multiply = multiply;
+	exports.divide = divide;
+
+})(module.exports);
